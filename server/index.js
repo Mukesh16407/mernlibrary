0ede46d7048ac8dd773e5c0ec7c81fc9ea468814
Mukesh16407 <mukesh.16407@gmail.com>
2023-05-02 00:20:51 +0530

    book router added

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 require("dotenv").config();
@@ -6,10 +6,8 @@ const connection = require("./config/db");
 const port = process.env.PORT || 5000;
 
 const usersRoute = require("./routes/userRouter");
-
-
-
+const booksRoute = require("./routes/booksRoute");
 
 app.use("/api/users", usersRoute);
-
+app.use("/api/books", booksRoute);
 app.listen(port, () => console.log(`Node server started at ${port}`));
