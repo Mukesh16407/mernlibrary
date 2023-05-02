b3528de32fa42dff2e26209d4b392c791d88c7e7
Mukesh16407 <mukesh.16407@gmail.com>
2023-05-03 01:22:16 +0530

    complete backend

--- server/index.js
+++ server/index.js
@@ -7,7 +7,12 @@ const port = process.env.PORT || 5000;
 
 const usersRoute = require("./routes/userRouter");
 const booksRoute = require("./routes/booksRoute");
+const issuesRoute = require("./routes/issuesRouter");
+const reportsRoute = require("./routes/reportsRoute");
 
 app.use("/api/users", usersRoute);
 app.use("/api/books", booksRoute);
+app.use("/api/issues", issuesRoute);
+app.use("/api/reports", reportsRoute);
+
 app.listen(port, () => console.log(`Node server started at ${port}`));
